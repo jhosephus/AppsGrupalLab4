7eb0fc4237d888c1d0d71fd0707002173cc41b3b
Jose Ruiz <ruizn.ja@pucp.pe>
2020-07-11 00:44:07 -0500

    publicar comentario: TEST y funcional

--- app/src/main/java/com/example/appsgrupallab4/AgregarComentario.java
+++ app/src/main/java/com/example/appsgrupallab4/AgregarComentario.java
@@ -1,5 +1,6 @@
 package com.example.appsgrupallab4;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,6 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
@@ -35,6 +37,8 @@ public class AgregarComentario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_comentario);
+
+        setActivity();
     }
 
     public void setActivity(){
@@ -50,6 +54,7 @@ public class AgregarComentario extends AppCompatActivity {
 
     public void publicarComentario(){
         String nombre = currentUser.getDisplayName();
+        //String nombre = "José";
         Date currentTime = Calendar.getInstance().getTime();
         String texto = comentario.getText().toString();
 
@@ -59,6 +64,8 @@ public class AgregarComentario extends AppCompatActivity {
         comentarioData.put("contenido", texto);
         comentarioData.put("userUID", currentUser.getUid());
 
+        postUID = "123456";
+
         db.collection("posts").document(postUID).collection("comentarios")
                 .add(comentarioData).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
             @Override
@@ -67,6 +74,11 @@ public class AgregarComentario extends AppCompatActivity {
                 setResult(RESULT_OK,new Intent());
                 finish();
 
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(AgregarComentario.this,"Ocurrió algo",Toast.LENGTH_SHORT).show();
             }
         });
 
