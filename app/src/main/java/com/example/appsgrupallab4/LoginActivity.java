0602f61c3d4e2ec01d6bb66b077c6b6d141aeacb
julioro <julio.rodriguezo@pucp.pe>
2020-07-11 10:23:12 -0500

    Manifest corregido

--- app/src/main/java/com/example/appsgrupallab4/LoginActivity.java
+++ app/src/main/java/com/example/appsgrupallab4/LoginActivity.java
@@ -107,6 +107,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void updateUI() {
+        Log.d("msgxd", "Logueandose ... ");
         startActivity(new Intent(LoginActivity.this, MainActivity.class));
 
     }
