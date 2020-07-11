74e72c7e32599c3be3ecbb6f9c937bebe6cf5b10
julioro <julio.rodriguezo@pucp.pe>
2020-07-11 00:21:37 -0500

    Facebook login basico

--- app/src/main/java/com/example/appsgrupallab4/LoginActivity.java
+++ app/src/main/java/com/example/appsgrupallab4/LoginActivity.java
@@ -10,17 +10,18 @@ import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
-
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsConstants;
 public class LoginActivity extends AppCompatActivity {
     private CallbackManager callbackManager;
     private LoginButton loginButton;
+    private CallbackManager mCallbackManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-
         callbackManager = CallbackManager.Factory.create();
         loginButton = (LoginButton) findViewById(R.id.login_button);
         loginButton.setReadPermissions("email");
