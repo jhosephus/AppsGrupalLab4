9abfa432c3365e810b5367c20843940fc7df77ec
JPVEGA <thekindofblizzard@gmail.com>
2020-07-11 13:38:48 -0500

    mostrar el recycle view con las imagenes , falta hacer el boton para q agrege comentarios

--- app/src/main/java/com/example/appsgrupallab4/PaginaPrincipal.java
+++ app/src/main/java/com/example/appsgrupallab4/PaginaPrincipal.java
@@ -1,5 +1,6 @@
 package com.example.appsgrupallab4;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -7,26 +8,78 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 
+import com.example.appsgrupallab4.entidades.Post;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
 import java.util.ArrayList;
 
 public class PaginaPrincipal extends AppCompatActivity {
-
+    FirebaseStorage firebaseStorage;
+    StorageReference storageReference;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     RecyclerView recyclerView;
     RecycleAdapter recycleAdapter;
-     ArrayList<String> comentario;
-     ArrayList<String> usuario;
-     ArrayList<Uri>  imagen;
-     Context context;
+    ArrayList<String> comentario = new ArrayList<String>();
+    ArrayList<String> usuario = new ArrayList<String>();
+    ArrayList<String> imagen = new ArrayList<String>();
+    File fileDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+    ArrayList<Post> posts = new ArrayList<Post>();
+    Post postAux = new Post();
+    int sais;
+    Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagina_principal);
-        recyclerView=findViewById(R.id.recycle);
-        recycleAdapter= new RecycleAdapter(comentario,usuario,imagen,PaginaPrincipal.this);
+        recyclerView = findViewById(R.id.recycle);
+
+        firebaseStorage = FirebaseStorage.getInstance();
+        storageReference = firebaseStorage.getReference();
+        db.collection("posts").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (DocumentSnapshot documentSnapshot : task.getResult()) {
+                        postAux = documentSnapshot.toObject(Post.class);
+                        postAux.setPostId(documentSnapshot.getId());
+                        posts.add(postAux);
+                    }
+                    comoseteantoje();
+                }
+            }
+        });
+
+
+
+
+
+    }
+
+    public void comoseteantoje() {
+        sais = posts.size();
+        for (int i = 0; i < sais; i++) {
+            imagen.add(posts.get(i).getUserUID() + "-" + posts.get(i).getPostId());
+            usuario.add(posts.get(i).getUserUID());
+            comentario.add(posts.get(i).getDescripcion());
+
+
+        }
+
+        recycleAdapter = new RecycleAdapter(comentario, usuario, imagen, PaginaPrincipal.this);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(recycleAdapter);
-
-
+        Log.d("sais", String.valueOf(sais));
     }
 }
--- app/src/main/java/com/example/appsgrupallab4/RecycleAdapter.java
+++ app/src/main/java/com/example/appsgrupallab4/RecycleAdapter.java
@@ -2,6 +2,7 @@ package com.example.appsgrupallab4;
 
 import android.content.Context;
 import android.net.Uri;
+import android.os.storage.StorageManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,10 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -19,14 +24,16 @@ import java.util.ArrayList;
 public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.ImageViewHolder> {
     private ArrayList<String> comentario;
     private ArrayList<String> usuario;
-    private ArrayList<Uri>  imagen;
+    private ArrayList<String> imagen;
     private Context context;
+    StorageReference storageReference;
+    FirebaseStorage storage = FirebaseStorage.getInstance();
 
-    public RecycleAdapter(ArrayList<String>  comentario, ArrayList<String>  usuario, ArrayList<Uri>  imagen,Context context){
-    this.context=context;
-    this.comentario=comentario;
-    this.usuario=usuario;
-    this.imagen=imagen;
+    public RecycleAdapter(ArrayList<String> comentario, ArrayList<String> usuario, ArrayList<String> imagen, Context context) {
+        this.context = context;
+        this.comentario = comentario;
+        this.usuario = usuario;
+        this.imagen = imagen;
 
     }
 
@@ -36,16 +43,26 @@ public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.ImageVie
     public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
 
-        View view=LayoutInflater.from(parent.getContext()).inflate(R.layout.pantalla_principal, parent, false);
-        ImageViewHolder imageViewHolder=new ImageViewHolder(view);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.pantalla_principal, parent, false);
+        ImageViewHolder imageViewHolder = new ImageViewHolder(view);
         return imageViewHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
-        Glide.with(context)
-                .load(imagen.get(position))
-                .into(holder.imageView1);
+    public void onBindViewHolder(@NonNull final ImageViewHolder holder, int position) {
+        storageReference = storage.getReference();
+
+        final StorageReference nose = storageReference.child(imagen.get(position) + ".jpg");
+        nose.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Glide.with(context)
+                        .load(uri)
+                        .into(holder.imageView1);
+
+            }
+        });
+
 
         holder.textView1.setText(usuario.get(position));
         holder.textView2.setText(comentario.get(position));
@@ -54,21 +71,21 @@ public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.ImageVie
     }
 
 
-
     @Override
     public int getItemCount() {
-        return usuario.size();
+        return imagen.size();
     }
 
     public class ImageViewHolder extends RecyclerView.ViewHolder {
         ImageView imageView1;
         TextView textView1;
         TextView textView2;
+
         public ImageViewHolder(@NonNull View itemView) {
             super(itemView);
             imageView1 = itemView.findViewById(R.id.imageView3);
             textView1 = itemView.findViewById(R.id.textView);
-            textView2= itemView.findViewById(R.id.textView2);
+            textView2 = itemView.findViewById(R.id.textView2);
         }
     }
 }
