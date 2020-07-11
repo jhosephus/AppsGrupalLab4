ef5278ca70c572a44002ee2d1bd5a0abfbe720bc
julioro <julio.rodriguezo@pucp.pe>
2020-07-11 00:41:03 -0500

    Facebook login basico

--- app/src/main/java/com/example/appsgrupallab4/LoginActivity.java
+++ app/src/main/java/com/example/appsgrupallab4/LoginActivity.java
@@ -1,10 +1,14 @@
 package com.example.appsgrupallab4;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -12,11 +16,20 @@ import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsConstants;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LoginActivity extends AppCompatActivity {
     private CallbackManager callbackManager;
     private LoginButton loginButton;
     private CallbackManager mCallbackManager;
 
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +39,15 @@ public class LoginActivity extends AppCompatActivity {
         loginButton = (LoginButton) findViewById(R.id.login_button);
         loginButton.setReadPermissions("email");
 
+        mAuth = FirebaseAuth.getInstance();
         // Callback registration
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 // App code
+                Log.d("msgxd", "Login ok");
+                handleFacebookAccessToken(loginResult.getAccessToken());
+
             }
 
             @Override
@@ -46,10 +63,51 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if (currentUser != null) updateUI();
+        // SI YA ESTA LOGEADO REDIRIGIRLO AL MAIN
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d("msgxd", "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("msgxd", "signInWithCredential:success");
+                            FirebaseUser currentUser = mAuth.getCurrentUser();
+
+                            // REDIRIGIRLO AL MAIN.
+                            if (currentUser != null) updateUI();
+                            updateUI();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.d("msgxd", "signInWithCredential:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void updateUI() {
+        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+
     }
 }
